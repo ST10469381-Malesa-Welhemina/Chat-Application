23f72ce6d97b3de4f7c7ca5cfd68f45501f56fde
pearl <pearl@MALESA>
2026-06-12 03:54:55 +0200

    my first commit

--- src/chatapp/ChatApp.java
+++ src/chatapp/ChatApp.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+
 public class ChatApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
